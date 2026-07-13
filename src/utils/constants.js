5767b9325ebca388aed6786ebefa5c128814d9c4
devme619 <f2016404p@alumni.bits-pilani.ac.in>
2026-07-13 13:32:46 +0530

    added memoization

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,8 +7,7 @@ export const API_OPTIONS = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNjkzZTllNTg1ZjQxNDRmNDljZWVjMTc2NjUxNGE1NCIsIm5iZiI6MTY2MTAyMjY2OC40MzIsInN1YiI6IjYzMDEzMWNjMzNhMzc2MDA4MjM1ZWM4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Nfj59uYlYdHJTgsMX6naV7eher6kvgZ7GdiD-qgOVrU",
+    Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_KEY}`,
   },
 };
 
